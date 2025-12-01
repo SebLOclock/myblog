5375b1f56b3aae9b9ffa3c38020e14c0a78988a0
Sèb <lemoine.sebastien.cdf@gmail.com>
2025-12-01 15:56:03 +0100

    resetup secrets

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <IntersectionObserver>
     <section id="about" class="section">
-      <h2 class="section__title">À propos </h2>
+      <h2 class="section__title">À propos</h2>
       <div class="about-content">
         <p>Manager passionné par l'innovation éducative, j'accompagne les talents vers l'excellence. Mon expérience en gestion de projets, pédagogie et dans le numérique me permettent d'inspirer et structurer des formations adaptées aux besoins des entreprises et des apprenants.</p>
         <p>Ancien développeur et consultant SI, j'ai une compréhension approfondie des enjeux techniques et managériaux qui me permettent d'apporter une valeur ajoutée significative dans la conception de parcours de formation et la gestion d'équipes.</p>
